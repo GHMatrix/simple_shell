809aaa356bb09f400280673cc650e332e6d12307
GHMatrix <gsmensah@yahoo.com>
2023-04-26 04:36:37 -0700

    Simple Shell Project main function

--- main.c
+++ main.c
@@ -1,51 +1,120 @@
-include "main.h"
+#include "main.h"
 /**
- * main - entry point to the program
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 success, 1 for erro
+ * main - initialize the variables of the program
+ * @argc: number of values received from the command line
+ * @argv: values received from the command line
+ * @env: number of values received from the command line
+ * Return: zero on succes.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[], char *env[])
 {
-	info_t info[] = {INFO_INIT};
-	int g_nana = 2;
+	prog_data data_struct = {NULL}, *data = &data_struct;
+	char *prompt = "";
 
-	/* Assembly code to amnipulate g_nana value */
-	__asm__ __volatile__(
-			"mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (g_nana)
-			: "r" (g_nana)
-			);
+	initialize_data(data, argc, argv, env);
 
-	if (argc == 2)
+	signal(SIGINT, handle_ctrl_c);
+
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
 	{
-		/* Open file in read-only mode */
-		g_nana = open(argv[1], O_RDONLY);
-
-		if (g_nana == -1)
-		{
-			/* Handle file opening errors */
-			if (errno == EACCESS)
-				exit(126);
-
-			if (errno == ENOENT)
-			{
-				_eputs(argv[0]);
-				_eputs(": 0: Cannot open");
-				_eputs(argv[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = g_nana;
+		errno = 2;
+		prompt = PROMPT_MSG;
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, argv);
-
-	return (EXIT_SUCCESS);
+	errno = 0;
+	sysinfo(prompt, data);
+	return (0);
 }
 
+/**
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
+ * @UNUSED: option of the prototype
+ */
+void handle_ctrl_c(int opr UNUSED)
+{
+	_print("\n");
+	_print(PROMPT_MSG);
+}
+
+/**
+ * initialize_data - inicialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments pased to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values received from the command line
+ */
+void initialize_data(prog_data *data, int argc, char *argv[], char **env)
+{
+	int i = 0;
+
+	data->prog_name = argv[0];
+	data->ip_line = NULL;
+	data->cmd_name = NULL;
+	data->cmd_counter = 0;
+
+	if (argc == 1)
+		data->file_desc = STDIN_FILENO;
+	else
+	{
+		data->file_desc = open(argv[1], O_RDONLY);
+		if (data->file_desc == -1)
+		{
+			_printe(data->prog_name);
+			_printe(": 0: Can't open ");
+			_printe(argv[1]);
+			_printe("\n");
+			exit(127);
+		}
+	}
+	data->tokens = NULL;
+	data->env = malloc(sizeof(char *) * 50);
+	if (env)
+	{
+		for (; env[i]; i++)
+		{
+			data->env[i] = str_duplicate(env[i]);
+		}
+	}
+	data->env[i] = NULL;
+	env = data->env;
+
+	data->aliases = malloc(sizeof(char *) * 20);
+	for (i = 0; i < 20; i++)
+	{
+		data->aliases[i] = NULL;
+	}
+}
+/**
+ * sysinfo - its a infinite loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
+ */
+void sysinfo(char *prompt, prog_data *data)
+{
+	int error_code = 0, string_len = 0;
+
+	while (++(data->cmd_counter))
+	{
+		_print(prompt);
+		error_code = string_len = _getline(data);
+
+		if (error_code == EOF)
+		{
+			free_all_data(data);
+			exit(errno);
+		}
+		if (string_len >= 1)
+		{
+			expand_alias(data);
+			expand_variables(data);
+			tokenize(data);
+			if (data->tokens[0])
+			{
+				error_code = execute(data);
+				if (error_code != 0)
+					_print_error(error_code, data);
+			}
+			free_recurrent_data(data);
+		}
+	}
+}
